902f450d7f894b54476921317ff670a8680fd333
Brian Russin <Brian Russin>
2017-02-24 12:06:52 -0500

    Added timeline and various data souce layers

---
+++ Assets/Scripts/CameraOrientedBehavior.cs
@@ -0,0 +1,21 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class CameraOrientedBehavior : MonoBehaviour {
+
+	// Use this for initialization
+	void Start () {
+		
+	}
+	
+	// Update is called once per frame
+	void Update () {
+        if( gameObject.activeInHierarchy )
+        {
+            Vector3 v = gameObject.transform.position - Camera.main.transform.position;
+            v.Normalize();
+            gameObject.transform.forward = v;
+        }
+    }
+}
---
+++ Assets/Scripts/RangeUtil.cs
@@ -0,0 +1,51 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class RangeUtil {
+
+    float minDomain = 0.0f;
+    float maxDomain = 1.0f;
+    float minRange = 0.0f;
+    float maxRange = 1.0f;
+
+    float conversionScale = 1.0f;
+
+
+    public RangeUtil()
+    {
+
+    }
+
+    public RangeUtil(float minD, float maxD, float minR, float maxR)
+    {
+        setDomain(minD, maxD);
+        setRange(minR, maxR);
+        setScale();
+    }
+
+    public void setDomain(float min, float max)
+    {
+        minDomain = min;
+        maxDomain = max;
+        setScale();
+    }
+
+    public void setRange(float min, float max)
+    {
+        minRange = min;
+        maxRange = max;
+
+        setScale();
+    }
+
+    private void setScale()
+    {
+        conversionScale = (maxRange - minRange) / (maxDomain - minDomain);
+    }
+
+    public float getVal(float d)
+    {
+        return (d - minDomain) * conversionScale + minRange;
+    }
+}
